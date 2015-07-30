df58e3c8f26111d8db343e5eecc45925079d714a
CarterBi <carterbian32@gmail.com>
2015-07-30 12:07:22 -0700

    Factoring Numbers

---
+++ Factor.py
@@ -0,0 +1,20 @@
+x = int(raw_input("What number do you want factored?"))
+
+factors = []
+
+end = int(x**0.5 + 1)
+
+
+for i in range(1, end + 1):
+    if x % i == 0:
+        factors.append(i)
+
+rev_factors = factors[::-1]
+
+for i in rev_factors:
+	new_factor = x/i
+	if new_factor not in factors:
+		factors.append(new_factor)
+
+
+print factors
